966c260d9d4c8cbfcf03a1b979186668f7aad142
Bitaltyn DEV <a2terminator@mail.ru>
2015-10-21 23:04:28 +0300

    changes

--- src/net.cpp
+++ src/net.cpp
@@ -1093,7 +1093,7 @@ void ThreadMapPort()
             }
         }
 
-        string strDesc = "Litecoin " + FormatFullVersion();
+        string strDesc = "Bitaltyn " + FormatFullVersion();
 
         try {
             loop {
@@ -1660,7 +1660,7 @@ bool BindListenPort(const CService &addrBind, string& strError)
     {
         int nErr = WSAGetLastError();
         if (nErr == WSAEADDRINUSE)
-            strError = strprintf(_("Unable to bind to %s on this computer. Litecoin is probably already running."), addrBind.ToString().c_str());
+            strError = strprintf(_("Unable to bind to %s on this computer. Bitaltyn is probably already running."), addrBind.ToString().c_str());
         else
             strError = strprintf(_("Unable to bind to %s on this computer (bind returned error %d, %s)"), addrBind.ToString().c_str(), nErr, strerror(nErr));
         printf("%s\n", strError.c_str());
