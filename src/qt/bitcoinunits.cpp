e5321e0d2caaeed669469bcb59421071acdd1d6c
Bitaltyn DEV <a2terminator@mail.ru>
2015-10-21 22:23:49 +0300

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("BTA");
+    case mBTC: return QString("mBTA");
+    case uBTC: return QString::fromUtf8("μBTA");
     default: return QString("???");
     }
 }
